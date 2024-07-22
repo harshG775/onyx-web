ad8a7627763ce33ed403c7f2db4498ad1079e232
Harsh <hgaur491@gmail.com>
2024-07-22 18:42:50 +0530

    fixes

--- src/app/(pages)/anime/_components/watch/PlayerControls.tsx
+++ src/app/(pages)/anime/_components/watch/PlayerControls.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEpState } from "@/components/providers/EpisodeState-provider";
 import { Button } from "@/components/ui/button";
+import SectionTitle from "@/components/ui/SectionTitle";
 import { EpisodesInfoType } from "@/services/gogoanime/GogoAnimeTypes";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
@@ -24,25 +25,32 @@ export default function PlayerControls({
     }, []);
     return (
         <section className="h-96 lg:h-full overflow-y-auto scrollbar-thin scrollbar-color">
-            <div className="sticky top-0 bg-background border-b-2">
-                Episode:{currentEp} {title}
-                <ul>
-                    {episodes.map((episode) => (
-                        <li key={episode.id}>
-                            <Button
-                                onClick={() => setCurrentEp(episode.number)}
-                                className={`${
-                                    episode.number === currentEp
-                                        ? "text-primary-foreground  bg-primary/80 hover:bg-primary/90"
-                                        : "text-secondary-foreground bg-secondary/50 hover:bg-primary/50 "
-                                } cursor-pointer text-sm px-2 py-1 w-full flex justify-start`}
-                            >
-                                Episode: {episode.number}
-                            </Button>
-                        </li>
-                    ))}
-                </ul>
+            <div className="sticky top-0 bg-background border-b-2 py-2">
+                <SectionTitle
+                    name="Playing"
+                    titleRight={<>Episode:{currentEp}</>}
+                />
+                <p className="text-wrap text-foreground/60">{title}</p>
             </div>
+            <ul className="space-y-1 py-1">
+                {episodes.map((episode) => (
+                    <li key={episode.id}>
+                        <Button
+                            onClick={() => {
+                                setCurrentEp(episode.number);
+                                router.push(`?ep=${episode.number}`);
+                            }}
+                            className={`${
+                                episode.number === currentEp
+                                    ? "text-primary-foreground  bg-primary/80 hover:bg-primary/90"
+                                    : "text-secondary-foreground bg-secondary/50 hover:bg-primary/50 "
+                            } cursor-pointer text-sm px-2 py-1 w-full flex justify-start`}
+                        >
+                            Episode: {episode.number}
+                        </Button>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 }
