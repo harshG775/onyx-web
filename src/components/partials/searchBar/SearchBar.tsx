29ca2ad8b029efa73db7ff26a886a842026ad67c
Harsh <hgaur491@gmail.com>
2024-08-20 17:41:43 +0530

    fixing navbar

--- src/components/partials/searchBar/SearchBar.tsx
+++ src/components/partials/searchBar/SearchBar.tsx
@@ -3,12 +3,16 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import useDebounce from "@/hooks/useDebounce";
 import Icon from "@/lib/icons/lucide";
+import { cn } from "@/lib/utils";
 import { useSearch } from "@/services/anilist/queries.tanstack";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function SearchBar() {
+type SearchBarProps = {
+    className?: string;
+};
+export default function SearchBar({ className }: SearchBarProps) {
     const router = useRouter();
     const [queryInput, setQueryInput] = useState("");
     const debounceSearch = useDebounce(queryInput, 1000);
@@ -21,7 +25,7 @@ export default function SearchBar() {
         router.push(`/search?query=${queryInput}`);
     };
     return (
-        <div className="relative">
+        <div className={cn("relative", className)}>
             <form className=" max-w-96 mx-auto" onSubmit={handleSearch}>
                 <Label htmlFor="search" className="sr-only">
                     search
@@ -46,41 +50,43 @@ export default function SearchBar() {
                     <Icon name="Search" className="w-8 h-8 p-1" />
                 </button>
             </form>
-            {inputFocus && status === "success" && queryInput.length != 0 && (
-                <div className="absolute left-0 right-0 top-12 bg-secondary p-2">
-                    <ul>
-                        {data?.data?.Page?.media?.map((anime: any) => (
-                            <li key={anime.id}>
-                                <Link
-                                    href={`anime/${anime.id}`}
-                                    className=" flex gap-2 bg-background/40 hover:bg-primary/20 p-1"
-                                >
-                                    <img
-                                        src={anime.coverImage.medium}
-                                        alt={anime.title.userPreferred}
-                                        width={50}
-                                        height={50}
-                                    />
-                                    <p className="line-clamp-2">
-                                        <span className="text-xs">
-                                            {anime.title.userPreferred}
-                                        </span>
-                                    </p>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="bg-secondary/50 py-1">
-                        <Link
-                            href={"#"}
-                            // href={`/search?query=${queryInput}`}
-                            className="text-ring hover:text-primary"
-                        >
-                            ViewMore
-                        </Link>
+            {inputFocus &&
+                status === "success" &&
+                data?.data?.Page?.media?.length > 0 && (
+                    <div className="absolute left-0 right-0 top-12 bg-secondary p-2">
+                        <ul>
+                            {data?.data?.Page?.media?.map((anime: any) => (
+                                <li key={anime.id}>
+                                    <Link
+                                        href={`anime/${anime.id}`}
+                                        className=" flex gap-2 bg-background/40 hover:bg-primary/20 p-1"
+                                    >
+                                        <img
+                                            src={anime.coverImage.medium}
+                                            alt={anime.title.userPreferred}
+                                            width={50}
+                                            height={50}
+                                        />
+                                        <p>
+                                            <span className="line-clamp-2">
+                                                {anime.title.userPreferred}
+                                            </span>
+                                        </p>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="bg-secondary/50 py-1">
+                            <Link
+                                href={"#"}
+                                // href={`/search?query=${queryInput}`}
+                                className="text-ring hover:text-primary"
+                            >
+                                ViewMore
+                            </Link>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
         </div>
     );
 }
--- src/components/partials/topNavbar/TopNavbar.tsx
+++ src/components/partials/topNavbar/TopNavbar.tsx
@@ -1,4 +1,7 @@
+"use client";
+import Icon from "@/lib/icons/lucide";
 import SearchBar from "../searchBar/SearchBar";
+import { useState } from "react";
 
 const navLinksData = [
     {
@@ -7,16 +10,20 @@ const navLinksData = [
     },
 ];
 export default function TopNavbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className="bg-background sticky top-0 z-50">
-            <ul className="flex justify-between p-2">
-                <li>Onyxstream</li>
+            <ul className="flex justify-between items-center gap-2  p-4 container">
+                <li className="font-bold text-base sm:text-xl">
+                    <span>Ony</span>
+                    <span className="text-primary/80">xstream</span>
+                </li>
+                {/* {navLinksData.map((el) => (
+                    <li key={el.href}>{el.title}</li>
+                ))} */}
                 <li>
                     <SearchBar />
                 </li>
-                {navLinksData.map((el) => (
-                    <li key={el.href}>{el.title}</li>
-                ))}
             </ul>
         </nav>
     );
