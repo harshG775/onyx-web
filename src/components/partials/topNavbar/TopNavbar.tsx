4435b1358461834a7b90e6ea752904eda37dda26
Harsh <hgaur491@gmail.com>
2024-08-20 17:47:31 +0530

    ///

--- src/components/partials/topNavbar/TopNavbar.tsx
+++ src/components/partials/topNavbar/TopNavbar.tsx
@@ -2,6 +2,7 @@
 import Icon from "@/lib/icons/lucide";
 import SearchBar from "../searchBar/SearchBar";
 import { useState } from "react";
+import Link from "next/link";
 
 const navLinksData = [
     {
@@ -13,10 +14,15 @@ export default function TopNavbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className="bg-background sticky top-0 z-50">
-            <ul className="flex justify-between items-center gap-2  p-4 container">
-                <li className="font-bold text-base sm:text-xl">
-                    <span>Ony</span>
-                    <span className="text-primary/80">xstream</span>
+            <ul className="flex justify-between items-center gap-2  py-2 px-4 container">
+                <li>
+                    <Link
+                        href={"/home"}
+                        className="block font-bold text-base sm:text-xl"
+                    >
+                        <span>Ony</span>
+                        <span className="text-primary/80">xstream</span>
+                    </Link>
                 </li>
                 {/* {navLinksData.map((el) => (
                     <li key={el.href}>{el.title}</li>
